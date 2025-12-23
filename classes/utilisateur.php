924d14e99ae40ffeba9c6a4ca31747ffe13c2700
ZiyadCh <chetoziyad@gmail.com>
2025-12-23 09:42:35 +0100

    getters

--- classes/utilisateur.php
+++ classes/utilisateur.php
@@ -1,8 +1,16 @@
 <?php 
 class user{
     protected $id;
+    protected $nom;
+    protected $prenom;
     protected $email;
-    protected $password;
+    protected $passwordhash;
+
+    public function getId() { return $this->id; }
+    public function getNom() { return $this->nom; }
+    public function getPrenom() { return $this->prenom; }
+    public function getEmail() { return $this->email; }
+
 }
 class coach extends user{
     protected $discipline;
