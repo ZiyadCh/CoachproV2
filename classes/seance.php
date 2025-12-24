9071375262f98772e7ada65492a7d3b7f103a08f
ZiyadCh <chetoziyad@gmail.com>
2025-12-24 18:22:30 +0100

    seance getter settter

--- classes/seance.php
+++ classes/seance.php
@@ -13,7 +13,44 @@ class seance {
         $this->heure = $heure;
         $this->duree = $duree;
         $this->statut = $statut;
+    }
     
+    //getters
+    public function getCid()
+    {
+        return $this->coach_id;
+    }
+    public function getDate()
+    {
+        return $this->date;
+    }
+    public function getHeure()
+    {
+        return $this->heure;
+    }
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+    //setters
+    public function setCid($coach_id){
+        $this->coach_id = $coach_id;
+    }
+    public function setDate($date){
+        $this->date = $date;
+    }
+    public function setHeure($heure){
+        $this->heure = $heure;
+    }
+    public function setDuree($duree){
+        $this->duree = $duree;
+    }
+    public function setStatut($statut){
+        $this->statut = $statut;
     }
 }
 ?>
